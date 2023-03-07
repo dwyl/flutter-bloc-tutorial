9ced9c817f3a1de35beb452918a5da7e8f8d918d
Luís Arteiro <lucho14garfield@gmail.com>
2023-03-07 10:31:10 +0000

    feat: Adding new todo is now possible. #1

--- lib/main.dart
+++ lib/main.dart
@@ -2,69 +2,82 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo/bloc/todo_bloc.dart';
 import 'package:todo/todo.dart';
+import 'package:uuid/uuid.dart';
+
+Uuid uuid = Uuid();
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatefulWidget {
+class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
-  State<MainApp> createState() => _MainAppState();
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => TodoBloc(),
+      child: HomePage(),
+    );
+  }
 }
 
-class _MainAppState extends State<MainApp> {
+class HomePage extends StatelessWidget {
+  TextEditingController txtFieldController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: BlocProvider(
-      create: (context) => TodoBloc(),
-      child: Scaffold(
-          body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-        children: [
-          // Textfield to add new todo item
-          TextField(
-            decoration: const InputDecoration(
-              labelText: 'What do we need to do?',
-            ),
-            onSubmitted: (value) {
-              print("submit new todo:$value");
-            },
+        home: Scaffold(
+            body: ListView(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+      children: [
+        // Textfield to add new todo item
+        TextField(
+          controller: txtFieldController,
+          decoration: const InputDecoration(
+            labelText: 'What do we need to do?',
           ),
+          onSubmitted: (value) {
+            // Create new item and create AddTodo event
+            TodoItem newTodoItem = TodoItem(description: value, id: uuid.v4());
+            BlocProvider.of<TodoBloc>(context).add(AddTodoEvent(newTodoItem));
 
-          const SizedBox(height: 42),
+            // Clear textfield
+            txtFieldController.clear();
+          },
+        ),
 
-          // Title for items left
-          BlocBuilder<TodoBloc, TodoState>(
-            builder: (context, state) {
-              int numItemsLeft = state.items.where((element) => element.completed).length;
+        const SizedBox(height: 42),
 
-              return Padding(
-                padding: const EdgeInsets.only(bottom: 16.0),
-                child: Text('${numItemsLeft} items left', style: const TextStyle(fontSize: 20)),
-              );
-            },
-          ),
+        // Title for items left
+        BlocBuilder<TodoBloc, TodoState>(
+          builder: (context, state) {
+            int numItemsLeft = state.items.where((element) => element.completed).length;
 
-          // List of items
-          BlocBuilder<TodoBloc, TodoState>(
-            builder: (context, state) {
-              List<TodoItem> items = state.items;
+            return Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: Text('${numItemsLeft} items left', style: const TextStyle(fontSize: 20)),
+            );
+          },
+        ),
 
-              return ListView(
-                shrinkWrap: true,
-                children: [
-                  if (items.isNotEmpty) const Divider(height: 0),
-                  for (var i = 0; i < items.length; i++) ...[if (i > 0) const Divider(height: 0), ItemCard(item: items[i])],
-                ],
-              );
-            },
-          ),
-        ],
-      )),
-    ));
+        // List of items
+        BlocBuilder<TodoBloc, TodoState>(
+          builder: (context, state) {
+            List<TodoItem> items = state.items;
+
+            return ListView(
+              shrinkWrap: true,
+              children: [
+                if (items.isNotEmpty) const Divider(height: 0),
+                for (var i = 0; i < items.length; i++) ...[if (i > 0) const Divider(height: 0), ItemCard(item: items[i])],
+              ],
+            );
+          },
+        ),
+      ],
+    )));
   }
 }
 
